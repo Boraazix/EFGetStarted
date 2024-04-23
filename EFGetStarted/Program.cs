d6f8152a562c58dedfc516a485dc6980eaf7ab24
Russell <russell.ed.123@gmail.com>
2024-04-23 18:07:34 -0300

    it works

---
+++ EFGetStarted/Program.cs
@@ -0,0 +1,29 @@
+﻿using EFGetStarted;
+
+using (var context = new Repository())
+{
+    // create if not exists
+    context.Database.EnsureCreated();
+
+    // create entity objects
+    Blog blog = new Blog() { Url = "www.com.vc" };
+    Post post = new Post() { Title = "A verdade", Content = "nua e crua", Blog = blog };
+    blog.Posts.Add(post);
+
+    // add entity to the context
+    context.Blogs.Add(blog);
+    context.Posts.Add(post);
+
+    // save data to the database tables
+    context.SaveChanges();
+
+    // retrive data
+    foreach(Post p in context.Posts)
+    {
+        Console.WriteLine(p.Title + ", " + p.Content);
+    }
+    foreach(Blog b in context.Blogs)
+    {
+        Console.WriteLine(b.Url);
+    }
+}
---
+++ EFGetStarted/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFGetStarted
+{
+    public class Repository : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=efgetstarted;Trusted_Connection=True;Encrypt=false;");
+        }
+    }
+}
